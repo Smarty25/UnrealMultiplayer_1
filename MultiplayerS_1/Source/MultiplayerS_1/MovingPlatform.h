1b7f251419d090a20ee566db8532ab189c89b5ef
Chase <smarty0425@gmail.com>
2018-11-12 10:23:11 -0700

    Made a moving platform that only moves for server

---
+++ MultiplayerS_1/Source/MultiplayerS_1/MovingPlatform.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERS_1_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	AMovingPlatform();
+
+	UPROPERTY(EditAnywhere)
+		float MoveSpeed = .5;
+	
+protected:
+	virtual void Tick(float DeltaTime) override;
+	
+	
+};
