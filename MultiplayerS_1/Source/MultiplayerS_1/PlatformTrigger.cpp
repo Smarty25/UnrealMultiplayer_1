02f9a6c5ce2a964b2e6e6c2b01899dbaf5d07df6
Chase <smarty0425@gmail.com>
2018-11-19 09:15:27 -0700

    Settup begin and end overlap events on the PlatformTrigger

--- MultiplayerS_1/Source/MultiplayerS_1/PlatformTrigger.cpp
+++ MultiplayerS_1/Source/MultiplayerS_1/PlatformTrigger.cpp
@@ -20,6 +20,8 @@ void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
@@ -27,5 +29,15 @@ void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated"))
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"))
 }
 
--- MultiplayerS_1/Source/MultiplayerS_1/PlatformTrigger.h
+++ MultiplayerS_1/Source/MultiplayerS_1/PlatformTrigger.h
@@ -26,5 +26,10 @@ public:
 private:
 	UPROPERTY(VisibleAnywhere)
 	class UBoxComponent* TriggerVolume;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
